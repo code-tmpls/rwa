7d53afe07739be875cb605e2221d689905e1c207
Nellutla L N Rao <nlnrao228@gmail.com>
2023-08-14 20:28:59 -0700

    Added Splash Screen

--- apt-fe/src/config/Routing.js
+++ apt-fe/src/config/Routing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Welcome from "@Pages/Welcome/index.js";
 import Home from "@Pages/Home/index.js";
 import Exam from "@Pages/Exam/index.js";
 import Menu2 from "@Pages/Menu2/index.js";
@@ -8,7 +9,8 @@ import Menu3 from "@Pages/Menu3/index.js";
 export const AppRouting = ()=>{
  return (<BrowserRouter basename="/">
     <Routes>
-      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/" element={<Welcome/>} />
+      <Route exact path="/home" element={<Home/>} />
       <Route exact path="/exam" element={<Exam />} />
       <Route exact path="/menu2" element={<Menu2 />} />
       <Route exact path="/menu3" element={<Menu3 />} />
