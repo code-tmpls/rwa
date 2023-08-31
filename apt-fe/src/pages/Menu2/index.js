daf8862ea0dc4a8992cd7383feb5aec6fc955320
AdithyaKankipati <adithyakankipati28@gmail.com>
2023-08-31 10:29:08 +0530

    Added edit Profile

--- apt-fe/src/pages/Menu2/index.js
+++ apt-fe/src/pages/Menu2/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from '@Templates/Header/index.js';
 import { HeaderMenu } from '@Config/HeaderMenu.js';
-
+import { ContainerFluid, Row, Col } from "e-ui-react";
 const Menu2 = ()=>{
  
- return (<>
-  <Header menulinks={HeaderMenu} activeId="Menu2" />
+return (<>
+<Header menulinks={HeaderMenu} activeId="Menu2" /> 
 </>);
 };
 
