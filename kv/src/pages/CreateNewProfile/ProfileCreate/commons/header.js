44bfa25047c2a20aea421c6db5542e6d71db70c7
Nellutla L N Rao <nlnrao228@gmail.com>
2023-09-25 12:31:51 +0530

    Updated Create New Profile

--- kv/src/pages/CreateNewProfile/ProfileCreate/commons/header.js
+++ kv/src/pages/CreateNewProfile/ProfileCreate/commons/header.js
@@ -5,8 +5,8 @@ const Header = ({ title })=>{
     return (<div className="bs-profile-headers">
     <ContainerFluid>
         <Row>
-            <Col xl={8}>{title}</Col>
-            <Col xl={4}>
+            <Col xl={8} xxl={8}>{title}</Col>
+            <Col xl={4} xxl={4}>
                 <div className="mtop5p">
                     <ProgressBar type="danger" progress="55" height="8" />
                 </div>
